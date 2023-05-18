7b8e122e6c141789f4580251561412b3ab1ca798
tsurubaso <tsurubaso@gmail.com>
2023-05-18 15:50:17 +0900

    2:09 bouton fond vert

--- src/App.js
+++ src/App.js
@@ -168,17 +168,33 @@ function App() {
   };
 
   const addTask = () => {
-  const task={
-id: todoList.length===0? 1: todoList[todoList.length - 1].id+1 ,
-taskName: newTask
-  }
-  setTodoList([...todoList,task])
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false,
+    };
+    setTodoList([...todoList, task]);
   };
 
   const deleteTask = (id) => {
-   setTodoList(todoList.filter((task)=>task.id !==id ));
-    
+    setTodoList(todoList.filter((task) => task.id !== id));
   };
+
+
+  const completeTask=(id)=>{
+setTodoList(
+  todoList.map((task)=>{
+if(task.id===id){return {...task,completed:true} }else{
+  return task;
+}}
+
+
+  ))
+  };
+
+
+
+
   return (
     <div className="App">
       <div className="addTask">
@@ -187,10 +203,16 @@ taskName: newTask
       </div>
       <div className="list">
         {todoList.map((task) => {
-          return <Task taskName={task.taskName} id={task.id} deleteTask={deleteTask}/>
+          return (
+            <Task
+              taskName={task.taskName}
+              id={task.id}
+              completed={task.completed}
+              deleteTask={deleteTask}
+              completeTask={completeTask}
+            />
+          );
         })}
-
-         
       </div>
     </div>
   );
--- src/Task.js
+++ src/Task.js
@@ -1,7 +1,11 @@
 export const Task= (props) => {
     return (
-      <div className="task">
+      <div 
+      className="task"
+      style={{backgroundColor: props.completed?"green":"White"}}
+      >
         <h1>{props.taskName}</h1>
+        <button onClick={() => props.completeTask(props.id)}>Completed</button>
         <button onClick={() => props.deleteTask(props.id)}>x</button>
       </div>
     );
